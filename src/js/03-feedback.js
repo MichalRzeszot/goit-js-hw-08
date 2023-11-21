7374136c6a3c0e3b9a60faa8b068554592bd6a53
MichalRzeszot <134526382+MichalRzeszot@users.noreply.github.com>
2023-11-21 23:27:13 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+const localStorageKey = 'feedback-form-state';
+
+const saveFormState = () => {
+  const formData = {
+    email: feedbackForm.elements.email.value,
+    message: feedbackForm.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+};
+
+const loadFormState = () => {
+  const storedData = localStorage.getItem(localStorageKey);
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+    feedbackForm.elements.email.value = formData.email || '';
+    feedbackForm.elements.message.value = formData.message || '';
+  }
+};
+
+feedbackForm.addEventListener('input', throttle(saveFormState, 500));
+
+document.addEventListener('DOMContentLoaded', loadFormState);
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const storedData = localStorage.getItem(localStorageKey);
+
+  if (storedData) {
+    console.log('Submitted Data:', JSON.parse(storedData));
+  } else {
+    console.log('No data submitted.');
+  }
+
+  localStorage.removeItem(localStorageKey);
+  feedbackForm.reset();
+});
